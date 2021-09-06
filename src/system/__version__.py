e893adb9b4d8ff653088259bbefab9c118cdf73c
César Román <thecesrom@gmail.com>
2021-09-06 15:28:12 -0700

    refactor: add pylint
    add
    - .pylintrc
    - pylint pre-commit hook

---
+++ src/system/__version__.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+# Copyright (C) 2018-2021
+# Author: Cesar Roman
+# Contact: cesar@thecesrom.dev
+
+"""Package information."""
+
+__title__ = "ignition-api"
+__description__ = "Ignition Scripting API."
+__url__ = "https://github.com/thecesrom/Ignition/tree/8.0"
+__version__ = "8.0.17"
+__build__ = "2020111210"
+__author__ = "César Román"
+__author_email__ = "cesar@thecesrom.dev"
+__license__ = "MIT"
+__copyright__ = "Copyright (C) 2018-2021 César Román"
