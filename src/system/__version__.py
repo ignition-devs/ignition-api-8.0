24e5b9769f28e72f5c0989600644862a9d1aa6f8
César Román <thecesrom@gmail.com>
2021-12-07 08:41:27 -0800

    chore(release): prepare for v8.0.17.post4 redux

--- src/system/__version__.py
+++ src/system/__version__.py
@@ -1,13 +1,7 @@
 # coding=utf-8
 """Package information."""
 
-__title__ = "ignition-api"
-__description__ = "Ignition Scripting API."
-__url__ = "https://github.com/thecesrom/Ignition/tree/8.0"
 __version__ = "8.0.17"
-__cycle__ = ".post4"
 __build__ = "2020111210"
-__author__ = "César Román"
-__author_email__ = "cesar@thecesrom.dev"
-__license__ = "MIT"
-__copyright__ = "Copyright (C) 2018-2021 César Román"
+__cycle__ = ".post4"
+__package_version__ = "{0}{1}".format(__version__, __cycle__)
